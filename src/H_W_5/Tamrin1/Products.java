87762062127147fdb338907bf627bfd882da5810
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 07:40:18 +0430

    Adds Properties Adds Constructor

--- src/H_W_5/Tamrin1/Products.java
+++ src/H_W_5/Tamrin1/Products.java
@@ -1,4 +1,13 @@
 package H_W_5.Tamrin1;
 
-public class Products {
+public abstract class Products {
+    protected String brand;
+    protected double price;
+
+    public Products(String brand, double price) {
+        this.brand = brand;
+        this.price = price;
+    }
+
+    
 }
