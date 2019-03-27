fd303b23d6c33d8da924f0471c3de8fc56c317bb
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 07:30:57 +0430

    Adds set Methods Adds set Information

--- src/H_W_5/Tamrin1/Person.java
+++ src/H_W_5/Tamrin1/Person.java
@@ -1,6 +1,10 @@
 package H_W_5.Tamrin1;
 
+import java.util.Scanner;
+
 public class Person {
+
+    Scanner scanner = new Scanner(System.in);
 
     private String userName;
     private String password;
@@ -15,5 +19,43 @@ public class Person {
         this.lastName = lastName;
     }
 
-    
+    public void setUserName() {
+        System.out.println("Please Enter your User Name:");
+        this.userName = scanner.next();
+    }
+
+    public void setPassword() {
+        System.out.println("Please Enter your Password:");
+        this.password = scanner.next();
+    }
+
+    public void setPhoneNumber() {
+        System.out.println("Please Enter your Phone number:");
+        this.phoneNumber = scanner.nextInt();
+    }
+
+    public void setEmail() {
+        System.out.println("Please Enter your Email:");
+        this.email = scanner.next();
+    }
+
+    public void setAddress() {
+        System.out.println("Please Enter your Province:");
+        String s = scanner.next() + " ";
+        System.out.println("Please Enter your Town:");
+        s += scanner.next() + " ";
+        System.out.println("Please Enter your Street:");
+        s += scanner.next() + " ";
+        System.out.println("Please Enter your Postal code:");
+        s += scanner.next() + " ";
+        this.address = s;
+    }
+
+    public void setInformation(){
+        setUserName();
+        setPassword();
+        setPhoneNumber();
+        setEmail();
+        setAddress();
+    }
 }
