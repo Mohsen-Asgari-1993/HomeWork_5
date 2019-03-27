f6a673a36c2bb3818b475f589af581a17ae63e2b
mohsen <mohsen1993asgari@gmail.com>
2019-03-27 07:25:22 +0430

    Creates Person Class Adds Properties Adds Constructor

---
+++ src/H_W_5/Tamrin1/Person.java
@@ -0,0 +1,19 @@
+package H_W_5.Tamrin1;
+
+public class Person {
+
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String email;
+    private String address;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    
+}
